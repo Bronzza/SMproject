71634f9952425be97e972c0dc89b590766986e4d
Sergey Bronzza <repinetskiy@gmail.com>
2018-09-23 22:13:29 +0300

    Registration page and login page commit

---
+++ src/main/java/applicationPackage/bins/RegistrationPage.java
@@ -0,0 +1,119 @@
+package applicationPackage.bins;
+
+import applicationPackage.Enums.AccessRights;
+import applicationPackage.entitys.Customer;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+import java.sql.Date;
+
+import static applicationPackage.Enums.AccessRights.VIEWER;
+
+@Named
+public class RegistrationPage {
+    private String loginRegistration;
+    private String passwordRegistration;
+    private AccessRights accessRights = VIEWER;
+
+    private String Name;
+    private String SurName;
+    private String emailRegistration;
+    private String telNumberRegistration;
+    private Date birthdayRegistration;
+    private boolean isManReg;
+
+    public String getLoginRegistration() {
+        return loginRegistration;
+    }
+
+    public void setLoginRegistration(String loginRegistration) {
+        this.loginRegistration = loginRegistration;
+    }
+
+    public String getPasswordRegistration() {
+        return passwordRegistration;
+    }
+
+    public void setPasswordRegistration(String passwordRegistration) {
+        this.passwordRegistration = passwordRegistration;
+    }
+
+    public AccessRights getAccessRights() {
+        return accessRights;
+    }
+
+    public void setAccessRights(AccessRights accessRights) {
+        this.accessRights = accessRights;
+    }
+
+    public String getEmailRegistration() {
+        return emailRegistration;
+    }
+
+    public void setEmailRegistration(String emailRegistration) {
+        this.emailRegistration = emailRegistration;
+    }
+
+    public String getTelNumberRegistration() {
+        return telNumberRegistration;
+    }
+
+    public void setTelNumberRegistration(String telNumberRegistration) {
+        this.telNumberRegistration = telNumberRegistration;
+    }
+
+    public Date getBirthdayRegistration() {
+        return birthdayRegistration;
+    }
+
+    public void setBirthdayRegistration(Date birthdayRegistration) {
+        this.birthdayRegistration = birthdayRegistration;
+    }
+
+    public boolean isManReg() {
+        return isManReg;
+    }
+
+    public void setManReg(boolean manReg) {
+        isManReg = manReg;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSurName() {
+        return SurName;
+    }
+
+    public void setSurName(String surName) {
+        SurName = surName;
+    }
+
+    public String saveRegistrationInfo() {
+        Customer customerTemp = new Customer();
+        customerTemp.setLogin(getLoginRegistration());
+        customerTemp.setPassword(getPasswordRegistration());
+        customerTemp.setName(getName());
+        customerTemp.setSurName(getSurName());
+        customerTemp.setTelNumber(getTelNumberRegistration());
+        customerTemp.setEmail(getEmailRegistration());
+        customerTemp.setBirthday(getBirthdayRegistration());
+        customerTemp.setMan(isManReg());
+        sendMessage("Going to Main");
+        return "goToMain";
+
+    }
+
+    public void sendMessage(String message) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(message, null));
+//        context.addMessage(null, new FacesMessage("Second Message", "Additional Message Detail"));
+    }
+}
